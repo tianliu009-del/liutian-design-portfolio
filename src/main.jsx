95c8fb8532c674f5a2981b25582ea379908c7b4f
tianliu009-del <tianliu009@gmail.com>
2026-07-12 13:24:32 +0800

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import React from "react";\nimport ReactDOM from "react-dom/client";\nimport App from "./App.jsx";\nimport "./styles.css";\n\nReactDOM.createRoot(document.getElementById("root")).render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>\n);\n
